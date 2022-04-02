2d39a843390f91322ab18ce77f94c197a4d641ac
Corsenac Pierre <pierre.corsenac@epitech.eu>
2022-04-02 10:42:01 +0200

    [FIX] [SERVICE] : removing useless functions

--- PierreTestLBC/Service/Service.swift
+++ PierreTestLBC/Service/Service.swift
@@ -39,22 +39,4 @@ class Service {
     public func loadCategories(url: String, completion: @escaping ((Result<[Category], ErrorNetwork>) -> Void)) {
         fetchData(url: url, completion: completion)
     }
-    
-    func fetchCategoryData(url: String, completion: @escaping ([Category]) -> Void) {
-        var categories : [Category] = []
-        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
-            guard let data = data, error == nil else {
-                return
-            }
-            
-            do {
-                categories = try JSONDecoder().decode([Category].self, from: data)
-                completion(categories)
-            } catch {
-                print(error.self)
-                print("failed to JSON DECODE \(error.localizedDescription)")
-            }
-        })
-        task.resume()
-    }
 }
